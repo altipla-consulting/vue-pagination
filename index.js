a8cbb7e323ade7097eb3b6a62d141d8846407308
Alberto Moreno <albertinivva@gmail.com>
2020-07-21 17:01:32 +0200

    Expone si estamos en la primera o última página. (#2)

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ export function useClientAsyncPagination({pageSize = DEFAULT_PAGE_SIZE, fetch}) 
   let hasPrevPage = computed(() => start.value > 0)
   let isEmpty = computed(() => !firstUpdate.value && !totalSize.value)
   let totalPages = computed(() => Math.ceil(totalSize.value / pageSize.value) - 1)
+  let isFirstPage = computed(() => page.value === 1)
+  let isLastPage = computed(() => page.value === totalPages.value)
   let items = computed(() => {
     if (loading.value && !Object.keys(cache.value).length) {
       return []
@@ -125,6 +127,8 @@ export function useClientAsyncPagination({pageSize = DEFAULT_PAGE_SIZE, fetch}) 
     prevPage,
     firstPage,
     lastPage,
+    isFirstPage,
+    isLastPage,
     mounted,
     pageSize,
 
@@ -147,7 +151,9 @@ export function useControlledPagination({fetch}) {
   let hasNextPage = computed(() => end.value < totalSize.value)
   let hasPrevPage = computed(() => start.value > 0)
   let isEmpty = computed(() => !firstUpdate.value && !totalSize.value)
-  let totalPages = computed(() => Math.ceil(totalSize.value / pageSize.value.value) - 1)
+  let totalPages = computed(() => Math.ceil(totalSize.value / pageSize.value) - 1)
+  let isFirstPage = computed(() => page.value === 1)
+  let isLastPage = computed(() => page.value === totalPages.value)
   let items = ref([])
 
   async function goTo(newPage, doNotScroll) {
@@ -199,6 +205,8 @@ export function useControlledPagination({fetch}) {
     hasPrevPage,
     firstPage,
     lastPage,
+    isFirstPage,
+    isLastPage,
     isEmpty,
     totalPages,
     items,
