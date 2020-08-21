80d76ff33301fbd8306a117470a3c15ac9a3a744
Ernesto Alejo <ernestokarim@gmail.com>
2020-08-21 19:41:18 +0200

    Prueba la paginación client async solucionando un error en la lógica de la última página.

---
+++ src/Pagination.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <header>
+    <i>{{model.loading ? 'Loading...' : 'Success'}}</i>
+    <p>
+      <button @click="model.firstPage" :disabled="model.isFirstPage"><strong>First page</button>
+      &nbsp;
+      <button @click="model.prevPage" :disabled="!model.hasPrevPage"><strong>Prev page</button>
+      &nbsp;
+      <button @click="model.nextPage" :disabled="!model.hasNextPage"><strong>Next page</button>
+      &nbsp;
+      <button @click="model.lastPage" :disabled="model.isLastPage"><strong>Last page</button>
+    </p>
+  </header>
+  <p>
+    <strong>Pagination: {{model.totalSize ? model.start+1 : model.start}} - {{model.end}} of {{model.totalSize}}</strong>
+  </p>
+  <p>
+    <code v-for="item in model.items" :key="item.key">{{item.key}}</code>
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    model: Object,
+  },
+}
+</script>
--- src/Test.vue
+++ src/Test.vue
@@ -1,17 +1,45 @@
 
 <template>
-  <div></div>
+  <h1>
+    useClientAsyncPagination
+    ( <a href="#" @click.prevent="fillClientAsync">fill data</a> )
+  </h1>
+  <Pagination :model="clientAsync"></Pagination>
 </template>
 
 <script>
-// import { useClientAsyncPagination, useControlledPagination } from '../lib'
+import { range } from 'lodash-es'
+
+import Pagination from './Pagination.vue'
+
+import { useClientAsyncPagination } from '../lib'
 
 
 export default {
-  name: 'vp-test',
+  components: {
+    Pagination,
+  },
+
+  data() {
+    return {
+      clientAsync: useClientAsyncPagination({
+        async fetch(keys) {
+          return keys.map(key => {
+            return { key }
+          })
+        },
+      }),
+    }
+  },
 
   mounted() {
     console.log('test')
+  },
+
+  methods: {
+    fillClientAsync() {
+      this.clientAsync.setKeys(range(1, 101))
+    },
   },
 }
 </script>
