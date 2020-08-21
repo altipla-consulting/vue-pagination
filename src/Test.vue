844d1b83c819dc7240db1e0acb30cc8a3fba8d6c
Ernesto Alejo <ernestokarim@gmail.com>
2020-08-21 19:15:33 +0200

    Integra el proyecto con vite para las pruebas.

---
+++ src/Test.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div></div>
+</template>
+
+<script>
+// import { useClientAsyncPagination, useControlledPagination } from '../lib'
+
+
+export default {
+  name: 'vp-test',
+
+  mounted() {
+    console.log('test')
+  },
+}
+</script>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import { createApp } from 'vue'
+
+import Test from './Test.vue'
+
+
+createApp(Test).mount('#app')
